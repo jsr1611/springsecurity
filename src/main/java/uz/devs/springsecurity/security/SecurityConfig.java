b2d99aabaca67435b02049a3881aa95393187b56
Jumanazar <jsr1611@outlook.com>
2022-03-25 16:57:53 +0900

    added password encoder

--- src/main/java/uz/devs/springsecurity/security/SecurityConfig.java
+++ src/main/java/uz/devs/springsecurity/security/SecurityConfig.java
@@ -1,13 +1,29 @@
 package uz.devs.springsecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfig(PasswordEncoder passwordEncoder){
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -18,5 +34,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails user_alitalib = User.builder()
+                .username("alitalib")
+                .password(passwordEncoder.encode("number1man"))
+                .roles("STUDENT")     //ROLE_STUDENT
+                .build();
+
+
+        return new InMemoryUserDetailsManager(
+                user_alitalib
+        );
     }
 }
