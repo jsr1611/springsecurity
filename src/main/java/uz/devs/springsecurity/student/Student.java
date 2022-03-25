ebe5e7d63d1ac19ac14af009a4ecee27aa904bc2
Jumanazar <jsr1611@outlook.com>
2022-03-25 16:09:31 +0900

    Initial commit

---
+++ src/main/java/uz/devs/springsecurity/student/Student.java
@@ -0,0 +1,14 @@
+package uz.devs.springsecurity.student;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Student {
+    private Integer studentId;
+    private String studentName;
+
+}
